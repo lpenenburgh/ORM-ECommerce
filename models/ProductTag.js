7122aaa7a3a3ee5a447218e1e77598ead1e710cc
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-06-02 15:00:09 -0600

    productag model cols added

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,34 @@
 const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../config/connection');
+const sequelize = require('../config/connection.js');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // define columns id(key), product id, tag id
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      //refers to the primary key (id) in the product model
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      //refers to the primary key (id) in the tag model
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
