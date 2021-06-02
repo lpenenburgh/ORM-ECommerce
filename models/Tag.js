937a468918cfe42d7694381ab4f522456aaf01de
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-06-02 14:51:08 -0600

    tag model cols added

--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,16 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    // define columns id(key) and name of tag
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
