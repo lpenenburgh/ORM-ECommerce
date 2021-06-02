d79b58d2a3f110a80f6b1cdc076eae8f2dfb3f3d
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-06-02 15:13:22 -0600

    product2catergory relationship

--- models/index.js
+++ models/index.js
@@ -4,7 +4,14 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+//create relationships between models using association methods and foreign keys
+
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  //referential action for a foreign key-deletes data from child tables automatically when you delete the data from the parent table. If the category is deleted, the product it belongs to will as well
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
 
