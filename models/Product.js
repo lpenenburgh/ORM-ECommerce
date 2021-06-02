65c733e2ad0c6fda64bddd07259585db7bf495d1
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-06-02 14:23:38 -0600

    product model-id/name columns

--- models/Product.js
+++ models/Product.js
@@ -1,6 +1,6 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
+// import  database connection from config.js
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
@@ -9,7 +9,17 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // define columns- set id as primary key for products
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
