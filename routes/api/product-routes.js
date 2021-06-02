ec3135e0970ed2ef23716941c4085d8f989aee00
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-06-02 17:16:15 -0600

    tag routes to review

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,10 +8,10 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
   // be sure to include its associated Category and Tag data
 router.get('/', async (req, res) => {
   try {
-    const productData = await Product.findAll({
+    const product = await Product.findAll({
       include: [{ model:Category , model: Tag, through: ProductTag }]
     });
-    res.status(200).json(productData);
+    res.status(200).json(product);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -20,16 +20,16 @@ router.get('/', async (req, res) => {
 // get one product
 router.get('/:id', async (req, res) => {
   try {
-    const productData = await Product.findByPk(req.params.id, {
+    const product = await Product.findByPk(req.params.id, {
       include: [{ model:Category, model: Tag, through: ProductTag }]
     });
 
-    if (!productData) {
-      res.status(404).json({ message: 'No product found with this id!' });
+    if (!product) {
+      res.status(404).json({ message: "No product found with this id" });
       return;
     }
 
-    res.status(200).json(productData);
+    res.status(200).json(product);
   } catch (err) {
     res.status(500).json(err);
   }
